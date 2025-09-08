300d813d6c2383e24c7b069b31574aa259a4e93a
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-09-08 11:08:47 -0600

    Agregar archivos de proyecto.

---
+++ ApexMagnamentFrontend/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ApexMagnamentFrontend.DTOs
+{
+    public class UserSession
+    {
+
+        public string User { get; set; } = null;
+        public string Password { get; set; } = null;
+
+
+    }
+}
