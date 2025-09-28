ad6d4aafa3d2ce8597c99afda0871cb0555cc997
Jasson Bladimir Navarro Giron <jassonnavarro460@gmail.com>
2025-09-28 15:09:50 -0600

    AsignacionDTO agregada

---
+++ ApexMagnamentFrontend/DTOs/AsignacionDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ApexMagnamentFrontend.DTOs
+{
+    public class AsignacionDTO
+    {
+        public int id { get; set; }
+
+        public int personalId { get; set; }
+        public int equipoId { get; set; }
+        public string equipoNombre { get; set; }
+        public string equipoDescripcion { get; set; }
+        public string equipoModelo{ get; set; }
+
+        public string equipoNserie { get; set; }
+
+    }
+}
